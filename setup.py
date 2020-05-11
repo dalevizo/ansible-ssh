0f3445c3dc14d6fe76fbe3534fdf8bca5dee4677
Frank Erens <frank.erens@androme.com>
2020-05-11 10:28:42 +0200

    Add GitHub URL to setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='ansible-ssh',  
+     name='ansible-ssh',
      version='0.9',
      scripts=['ansible-ssh'] ,
      author="Dimos Alevizos",
      author_email="dimos@alevizos.gr",
      description="Interactive SSH for Ansible",
      long_description=long_description,
+     url="https://github.com/dalevizo/ansible-ssh",
      packages=setuptools.find_packages(),
      classifiers=[
          "Programming Language :: Python :: 3",
